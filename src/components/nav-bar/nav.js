6aa95571b03f0ad6498ad909b38ce9449a123f43
akodali9 <akashkodali6@gmail.com>
2023-05-04 11:19:47 +0530

    skills pane added and scaled

--- src/components/nav-bar/nav.js
+++ src/components/nav-bar/nav.js
@@ -1,22 +1,45 @@
 import React from "react";
 import "./nav.css";
-import {NavLink } from "react-router-dom";function NavBar() {
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
   return (
     <>
-    {/* TODO Increase size for larger width devices and scale it up for smaller ones */}
+      {/* TODO Increase size for larger width devices and scale it up for smaller ones */}
       <div className="nav">
         <p className="logo">Akash Chowdary Kodali</p>
+
         <ul className="nav-links">
-          <NavLink to="/" className={({ isActive }) => (isActive ? "sub-linkactive sub-link" : "sub-link")}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "sub-linkactive sub-link" : "sub-link"
+            }
+          >
             Home
           </NavLink>
-          <NavLink to="/Projects" className={({ isActive }) => (isActive ? "sub-linkactive sub-link" : "sub-link")}>
+          <NavLink
+            to="/Projects"
+            className={({ isActive }) =>
+              isActive ? "sub-linkactive sub-link" : "sub-link"
+            }
+          >
             Projects
           </NavLink>
-          <NavLink to="/skills" className={({ isActive }) => (isActive ? "sub-linkactive sub-link" : "sub-link")}>
+          <NavLink
+            to="/skills"
+            className={({ isActive }) =>
+              isActive ? "sub-linkactive sub-link" : "sub-link"
+            }
+          >
             Skills
           </NavLink>
-          <NavLink to="/Education" className={({ isActive }) => (isActive ? "sub-linkactive sub-link" : "sub-link")}>
+          <NavLink
+            to="/Education"
+            className={({ isActive }) =>
+              isActive ? "sub-linkactive sub-link" : "sub-link"
+            }
+          >
             Education
           </NavLink>
         </ul>
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,9 +1,53 @@
 import React from "react";
+import "./css/Skills.css";
 
 export const Skills = () => {
-    return(
-        <div>
-            <h2>Skills page</h2>
-        </div>
-    );
-}
+  return (
+    <div className="mainS">
+      <p className="headingS">Languages</p>
+      <div className="detailsS">
+        <ul>
+          <li>C</li>
+          <li> Python</li>
+          <li>Java</li>
+          <li>R</li>
+        </ul>
+        <p className="headingS">Web</p>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+      </div>
+      <p className="headingS">Frameworks</p>
+      <div className="detailsS">
+        <ul>
+          <li>Flutter</li>
+          <li>React JS</li>
+        </ul>
+      </div>
+
+      <p className="headingS">Operating Systems</p>
+      <div className="detailsS">
+        <ul>
+          <li>Windows</li>
+          <li>MacOS</li>
+          <li>Linux</li>
+        </ul>
+      </div>
+      <p className="headingS">Tools</p>
+      <div className="detailsS">
+        <ul>
+          <li> Git </li>
+          <li> GitHub </li> 
+          <li> Visual Studio Code </li> 
+          <li> Eclipse </li>
+          <li>PyCharm </li> 
+          <li> Word </li>
+          <li>PowerPoint</li>
+          <li> Excel</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
