5ec05de1e99556954ff425a68fd457d931a4a3a1
akodali9 <akashkodali6@gmail.com>
2023-05-21 22:48:58 +0530

    scroll-to-top_addition

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
+import {React} from "react";
 import NavBar from "./nav-bar/nav";
 import { Routes,Route, HashRouter as Router } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Projects } from "./pages/Projects";
 import { Skills } from "./pages/Skills";
 import { Edu } from "./pages/Education";
+import ScrollToTop from "./pages/scrolltoTop";
 
 function App() {
   return (
     <div>
       <Router>
+        <ScrollToTop/>
         <NavBar />
         <Routes>
           <Route exact path = "/" element = {<Home/>}/>
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../pages/css/Skills.css";
+import "./css/Skills.css";
 
 export const Skills = () => {
   return (
