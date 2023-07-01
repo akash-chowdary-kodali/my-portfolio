1d53fcb4975e1898720499882556e26ea20a5f28
Akash Kodali <akashkodali6@gmail.com>
2023-07-02 00:26:51 +0530

    styled navbar blur

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -5,8 +5,20 @@ import favicon from "../../assets/icons/Ak.png";
 import { Link } from "react-scroll";
 import { useState } from "react";
 
-// import $ from "jquery";
+let prevScrollPos = window.scrollY;
 
+window.addEventListener("scroll", function () {
+  const currentScrollPos = window.scrollY;
+
+  if (prevScrollPos < currentScrollPos) {
+    document.querySelector(".nav").classList.add("hide");
+  } else {
+    document.querySelector(".nav").classList.remove("hide");
+  }
+
+  // update previous scroll position
+  prevScrollPos = currentScrollPos;
+});
 function NavBar() {
   const [toggle, settoggle] = useState(false);
 
@@ -14,131 +26,128 @@ function NavBar() {
     settoggle(!toggle);
   };
   return (
-      <div className={toggle ? "nav expanded" : "nav"} id="navigator">
-        <a href="https://akodali9.github.io/my-portfolio/">
-          <img src={favicon} alt="logo" className="logo" />
-        </a>
+    <div className={toggle ? "nav expanded" : "nav"} id="navigator">
+      <a href="https://akodali9.github.io/my-portfolio/">
+        <img src={favicon} alt="logo" className="logo" />
+      </a>
 
-        <div className={"nav-links"}>
-          <Link
-            smooth
-            spy
-            to="about"
-            className="sub-link"
-            activeClass="sub-linkactive"
-            offset={-70}
-            duration={1000}
-            onClick={handletoggle}
+      <div className={"nav-links"}>
+        <Link
+          smooth
+          spy
+          to="about"
+          className="sub-link"
+          activeClass="sub-linkactive"
+          offset={-70}
+          duration={1000}
+          onClick={handletoggle}
+        >
+          <span>01. </span>About
+        </Link>
+        <Link
+          smooth
+          spy
+          to="expertise"
+          className="sub-link"
+          activeClass="sub-linkactive"
+          offset={-70}
+          duration={1000}
+          onClick={handletoggle}
+        >
+          <span>02. </span>Expertise
+        </Link>
+        <Link
+          smooth
+          spy
+          to="works"
+          activeClass="sub-linkactive"
+          className="sub-link"
+          offset={-70}
+          duration={1000}
+          onClick={handletoggle}
+        >
+          <span>03. </span>Work
+        </Link>
+        <Link
+          smooth
+          spy
+          to="Experience"
+          activeClass="sub-linkactive"
+          className="sub-link"
+          offset={-70}
+          duration={1000}
+          onClick={handletoggle}
+        >
+          <span>04. </span>Experience
+        </Link>
+        <Link
+          smooth
+          spy
+          to="contact"
+          activeClass="sub-linkactive"
+          className="sub-link"
+          offset={-70}
+          duration={1000}
+          onClick={handletoggle}
+        >
+          <span>05. </span>Contact
+        </Link>
+      </div>
+      <div className="toggle-icon" onClick={handletoggle}>
+        {toggle ? (
+          <svg
+            width="50px"
+            height="50px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
           >
-            About
-          </Link>
-          <Link
-            smooth
-            spy
-            to="expertise"
-            className="sub-link"
-            activeClass="sub-linkactive"
-            offset={-70}
-            duration={1000}
-            onClick={handletoggle}
-          >
-            Expertise
-          </Link>
-          <Link
-            smooth
-            spy
-            to="works"
-            activeClass="sub-linkactive"
-            className="sub-link"
-            offset={-70}
-            duration={1000}
-            onClick={handletoggle}
-          >
-            Work
-          </Link>
-          <Link
-            smooth
-            spy
-            to="Experience"
-            activeClass="sub-linkactive"
-            className="sub-link"
-            offset={-70}
-            duration={1000}
-            onClick={handletoggle}
-          >
-            Experience
-          </Link>
-          <Link
-            smooth
-            spy
-            to="contact"
-            activeClass="sub-linkactive"
-            className="sub-link"
-            offset={-70}
-            duration={1000}
-            onClick={handletoggle}
-          >
-            Contact
-          </Link>
-        </div>
-        <div className="toggle-icon" onClick={handletoggle}>
-          {toggle ? (
-            <svg
-              width="40px"
-              height="40px"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path
+                d="M19 5L5 19M5.00001 5L19 19"
+                stroke="#6ce0dc"
+                strokeWidth="1.5"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                {" "}
+              ></path>
+            </g>
+          </svg>
+        ) : (
+          <svg
+            width="50px"
+            height="50px"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <g id="Menu / Menu_Alt_02">
                 <path
-                  d="M19 5L5 19M5.00001 5L19 19"
+                  id="Vector"
+                  d="M11 17H19M5 12H19M11 7H19"
                   stroke="#6ce0dc"
-                  strokeWidth="1.5"
+                  strokeWidth="2"
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                ></path>{" "}
+                ></path>
               </g>
-            </svg>
-          ) : (
-            <svg
-              width="40px"
-              height="40px"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g
-                id="SVGRepo_tracerCarrier"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              ></g>
-              <g id="SVGRepo_iconCarrier">
-                {" "}
-                <g id="Menu / Menu_Alt_02">
-                  {" "}
-                  <path
-                    id="Vector"
-                    d="M11 17H19M5 12H19M11 7H19"
-                    stroke="#6ce0dc"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  ></path>{" "}
-                </g>{" "}
-              </g>
-            </svg>
-          )}
-        </div>
+            </g>
+          </svg>
+        )}
       </div>
+    </div>
   );
 }
 
